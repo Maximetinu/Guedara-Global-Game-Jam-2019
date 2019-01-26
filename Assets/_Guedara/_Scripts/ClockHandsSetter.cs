de6febdc748bdf8596f9dead5acb771a73675f9b
Maximetinu <maximetinu@gmail.com>
2019-01-26 23:44:21 +0100

    Clock Simple anim script

---
+++ Assets/_Guedara/_Scripts/ClockHandsSetter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockHandsSetter : MonoBehaviour
+{
+    public SpriteRenderer minutesSpriteRenderer;
+    public Sprite[] minutesSprites;
+    public float quarterHourWaitTime = 60f;
+    
+    [Space(10)]
+    public bool overrideFirstAtStart = false;
+
+    int currentSprite = 0;
+    
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        if (overrideFirstAtStart && minutesSprites.Length >= 1)
+        {
+            minutesSpriteRenderer.sprite = minutesSprites[0];
+            currentSprite++;
+        }
+
+
+        while (minutesSprites.Length > currentSprite)
+        {
+            yield return new WaitForSeconds(quarterHourWaitTime);
+
+            minutesSpriteRenderer.sprite = minutesSprites[currentSprite];
+            currentSprite++;
+        }
+    }
+}
