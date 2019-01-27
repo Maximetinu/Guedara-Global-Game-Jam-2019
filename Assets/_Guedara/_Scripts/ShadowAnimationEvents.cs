55e8c2507bd43085eb0d5140ad29bf62d6e6bfbc
Maximetinu <maximetinu@gmail.com>
2019-01-27 13:58:03 +0100

    NO y SI de Winter (sin funcionalidad)

---
+++ Assets/_Guedara/_Scripts/ShadowAnimationEvents.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ShadowAnimationEvents : MonoBehaviour
+{
+    public UnityEvent onAnimationEnd;
+    
+    public void OnAnimationEnd()
+    {
+        onAnimationEnd.Invoke();
+    }
+}
