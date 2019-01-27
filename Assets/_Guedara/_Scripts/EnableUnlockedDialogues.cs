dbfe90acdf73983fac286784b196511a16e71cf0
Maximetinu <maximetinu@gmail.com>
2019-01-27 14:07:17 +0100

    Bugfixes autumn: unlock - lock + cat text

--- Assets/_Guedara/_Scripts/EnableUnlockedDialogues.cs
+++ Assets/_Guedara/_Scripts/EnableUnlockedDialogues.cs
@@ -1,17 +1,30 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
+    using UnityEngine.Events;
 
 public class EnableUnlockedDialogues : MonoBehaviour
 {
     public Button oldCoupleButton, coupleButton, petButton, familyButton;
-    
+
+    public UnityEvent ifNothingUnlocked;
+
+    public float nothingUnlockedWait = 2f;
+
     // Start is called before the first frame update
-    void Start()
+    IEnumerator Start()
     {
         petButton.interactable = UnlockDialogues.catUnlock;
         coupleButton.interactable = UnlockDialogues.coupleUnlock;
         oldCoupleButton.interactable = UnlockDialogues.oldCoupleUnlock;
         familyButton.interactable = UnlockDialogues.familyUnlock;
+
+        if (!UnlockDialogues.catUnlock && !UnlockDialogues.coupleUnlock && !UnlockDialogues.oldCoupleUnlock &&
+            !UnlockDialogues.familyUnlock)
+        {
+            yield return new WaitForSeconds(nothingUnlockedWait);
+            ifNothingUnlocked.Invoke();
+        }
     }
 
     void Update()
