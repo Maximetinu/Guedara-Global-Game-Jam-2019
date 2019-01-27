3771793bbeb1b77e8165b93881beed385ccce978
Maximetinu <maximetinu@gmail.com>
2019-01-27 15:08:12 +0100

    train sound fx + other fxs

---
+++ Assets/_Guedara/_Scripts/StepSounds.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class StepSounds : MonoBehaviour
+{
+    public AudioClip step;
+    AudioSource source;
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    public void PlayStep()
+    {
+        source.PlayOneShot(step);
+    }
+}
