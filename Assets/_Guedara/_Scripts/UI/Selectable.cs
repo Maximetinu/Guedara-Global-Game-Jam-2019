fbba7f37cf3ea7df51eae20f773a42be07df1b2e
Lifka <javierizquierdovera@gmail.com>
2019-01-27 03:41:22 +0100

    Improve bubble text

---
+++ Assets/_Guedara/_Scripts/UI/Selectable.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+
+[RequireComponent(typeof(Collider))]
+public class Selectable : MonoBehaviour, IPointerDownHandler
+{
+    public UnityEvent onSelect;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("On pointer down");
+        if (eventData.pointerCurrentRaycast.gameObject == gameObject)
+        {
+            onSelect.Invoke();
+        }
+    }
+
+}
