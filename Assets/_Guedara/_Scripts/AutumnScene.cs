e346c2c20e174b3b5c6eda4ee8c3b390b55ecc8a
Maximetinu <maximetinu@gmail.com>
2019-01-27 13:02:00 +0100

    Automun in progress

---
+++ Assets/_Guedara/_Scripts/AutumnScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AutumnScene : MonoBehaviour
+{
+    public UnityEvent onFadeHalf;
+    public UnityEvent onFadeEnd;
+
+    public void OnFadeHalf()
+    {
+        onFadeHalf.Invoke();
+    }
+
+    public void OnFadeEnd()
+    {
+        onFadeEnd.Invoke();
+    }
+}
---
+++ Assets/_Guedara/_Scripts/EnableUnlockedDialogues.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class EnableUnlockedDialogues : MonoBehaviour
+{
+    public Button oldCoupleButton, coupleButton, petButton, familyButton;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        petButton.interactable = UnlockDialogues.catUnlock;
+        coupleButton.interactable = UnlockDialogues.coupleUnlock;
+        oldCoupleButton.interactable = UnlockDialogues.oldCoupleUnlock;
+        familyButton.interactable = UnlockDialogues.familyUnlock;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            petButton.interactable = true;
+            coupleButton.interactable = true;
+            oldCoupleButton.interactable = true;
+            familyButton.interactable = true;
+        }
+    }
+}
